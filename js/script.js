953d84c530e425b051e874ee2a620c02f345fc7c
Ariel <117614710+RielDreams@users.noreply.github.com>
2022-12-04 16:26:01 -0500

    completing queue, adding localstorage

--- js/script.js
+++ js/script.js
@@ -3,12 +3,11 @@
 ///////////
 // $(function(){ 
 
-
 ///////////////
 //ELEMENTS
 ///////////////
     let limit = 500;
-    let xr = 0;
+    const $body = $('body')
     const $poster = $('#poster');
     const $title = $('#title');
     const $epin = $('#epiN');
@@ -21,7 +20,7 @@
     const $section = $('#showroom');
     const $random = $('#random');
     const $hidden = $('#hidden');
-    const $randomb = $('#randomButton');
+    const $dq = $('#DQueue')
 
 ///////////////
 //VARIABLES
@@ -39,10 +38,16 @@ const headers = {
 ///////////////
     $form.on('submit', getData)
     $random.click(randomClick)
+    $(document).on('click', '#add', addToQue);
+    $(document).on('click', '#remove', removeFromQue)
 
 ///////////////
 //FUNCTIONS
 ///////////////
+
+    ///////////////
+    //search bar function
+    ///////////////
     function getData(e){
         e.preventDefault();
         userinput = $input.val();
@@ -50,44 +55,39 @@ const headers = {
         $.ajax({url: url+userinput, headers}).then(
             (data) => {
                 render(data.data)
-                // console.log(data)
+                console.log(data)
             }, (error) => {console.log("this isnt working", error)
         })
     };
     function render(shows) {
         const showList = shows.map((list, i) => {  
-                return  `<div id="page">
-                <div id="poster" class="group">
-                    <a href="#ex${i}" rel="modal:open">
-                        <img id='movieImg' src="${list.image}"/>
-                    </a>
-                </div>
-                <div id="ex${i}" class="modal">
-                    <img id='movieImg' src="${list.image}"/>
-                    <p id="title" class="group">${list.title}</p>
-                    <p id="epiC" class="group">Episode Count:  ${list.episodes}</p>
-                    <p id="type"class="group">Type:   ${list.type}</p>
-                    <p id="air" class="group">Still Airing:   ${list.status}</p>
-                    <p id="synopsis" class="group">Summary:</p>
-                    <p id="sum" class="group">${list.synopsis}</p>
-                </div>
-                </div>`      
+            return  `<div id="page">
+                        <div id="poster" class="group">
+                            <a href="#ex${i}" rel="modal:open">
+                                <img id='movieImg' src="${list.image}"/>
+                            </a>
+                        </div>
+                        <div id="ex${i}" class="modal">
+                            <img id='movieImg' src="${list.image}"/>
+                            <p id="title" class="group">${list.title}</p>
+                            <p id="epiC" class="group">Episode Count:  ${list.episodes}</p>
+                            <p id="type"class="group">Type:   ${list.type}</p>
+                            <p id="air" class="group">Still Airing:   ${list.status}</p>
+                            <p id="synopsis" class="group">Summary:</p>
+                            <p id="sum" class="group">${list.synopsis}</p>
+                            <a href="#close-modal" rel="modal:close" class="buttons">
+                                <input id="add" class="button" type="button" value="Add" />
+                                <input id="remove" class="button" type="button" value="Remove" />
+                            </a>
+                        </div>
+                    </div>`      
         });
         $section.html(showList)
     }
 
-        ///////
-        //RANDOM DATA LOADS AUTOMATICALLY
-        //////
-
-    function randomClick() {
-        x = Math.floor(Math.random() * limit)
-        $('#randomButton').attr(`href`,`#x${x}`);
-        xr = xr + x
-        console.log(xr)
-        return xr = 0
-    }
-
+    ///////////////
+    //Random Data Auto Loading
+    ///////////////
     function randomData(){
         $.ajax({url, headers}).done(
             (data) => {
@@ -105,10 +105,64 @@ const headers = {
                     <p id="air" class="group">Still Airing:   ${list.status}</p>
                     <p id="synopsis" class="group">Summary:</p>
                     <p id="sum" class="group">${list.synopsis}</p>
+                    <a href="#close-modal" rel="modal:close">
+                        <input id="add" class="button" type="button" value="Add" />
+                        <input id="remove" class="button" type="button" value="Remove" />
+                    </a>
                 </div>
                 </div>`      
         });
         $hidden.html(randomEn)
     }
     randomData()
+
+    function randomClick(e) {
+        x = Math.floor(Math.random() * limit)
+        $('#randomButton').attr(`href`,`#x${x}`);
+    }
+    
+    ///////////////
+    //Add/remove to Queue
+    ///////////////
+
+    function addToQue(e) {
+        data = $(e.target).closest('div');
+        id = data[0].getAttribute('id');
+        d = data[0].children;
+       $dq.append(`<div id="page" class="${id}">
+                        <div id="poster" class="group">
+                            <a href="#${id}" rel="modal:open">${d[0].outerHTML}</a>
+                            <input id="remove" class="button" type="button" value="Remove" />
+                        </div>
+                    </div>`
+                    );
+        localStorage.setItem(id, d[0].outerHTML)
+    }
+
+    function removeFromQue(e) {
+        data = $(e.target).closest('div');
+        id = data[0].getAttribute('id');
+        // vvv outter remove button vvvv
+        d = data.closest(`#page`);
+        d.remove()
+        // vvv inner remove button 
+        $(`.${id}`).remove()
+        console.log(d[0].className)
+        localStorage.removeItem(id)
+        localStorage.removeItem(id[0].className)
+    }
+
+    for(let key in localStorage){
+        if(localStorage.hasOwnProperty(key)){
+            $dq.append(`<div id="page" class="${key}">
+            <div id="poster" class="group">
+                <a href="#${key}" rel="modal:open">${localStorage[key]}</a>
+                <input id="remove" class="button" type="button" value="Remove" />
+            </div>
+            </div>`
+        );
+        console.log(`key ${key} . value ${localStorage[key]}`)
+        }
+    }
+
 // }
